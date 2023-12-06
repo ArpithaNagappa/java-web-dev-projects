72409661141d675258cfa21c7d99a11020af8929
Arpitha Nagappa <arpithagn021@gmail.com>
2023-12-06 11:14:07 -0600

    Committing

--- classes-part-one/exercises/src/main/java/Course.java
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -1,12 +1,10 @@
-package exercises.classespart1;
-
-import exercises.classespart2.Teacher;
+//import exercises.classespart2.Teacher;
 
 import java.util.ArrayList;
 
 public class Course {
         private String topic;
-        private Teacher instructor;
+        private String instructorName;
         private ArrayList<Student> enrolledStudents;
     }
-}
+
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,7 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student student = new Student("Appu", 1234,1,4.0);
+
     }
 }
