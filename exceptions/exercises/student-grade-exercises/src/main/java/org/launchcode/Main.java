91b13e1e5573a4138b0ed9117a45d5d7ed2e3207
Arpitha Nagappa <arpithagn021@gmail.com>
2024-01-11 16:32:09 -0600

    Committing

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,20 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        Scanner input;
+        input = new Scanner(System.in);
+        System.out.println("Enter the values for x : ");
+        int xValue = input.nextInt();
+        System.out.println("Enter the values for y : ");
+        int yValue = input.nextInt();
+        input.close();
+
+        Divide(xValue,yValue);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,15 +23,49 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+
+        System.out.println(CheckFileExtension(""));
     }
 
     public static void Divide(int x, int y)
     {
         // Write code here!
+        int result;
+        try {
+
+            if (y == 0) {
+                throw new ArithmeticException("Value of y can not be zero!");
+            } else {
+                result = x / y;
+                System.out.println("Result is : " + result);
+            }
+        }
+        catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
+    public static int CheckFileExtension(String fileName) {
         // Write code here!
+        int numberOfPointRecieved = 0;
+
+            try {
+
+                if (fileName.contains(".java")) {
+                    numberOfPointRecieved = 1;
+                } else if (fileName.contains(".java")) {
+                    numberOfPointRecieved = 0;
+                } else if (fileName.contains("")) {
+                    numberOfPointRecieved = -1;
+                    throw new NullPointerException("File should be in .java format");
+                }
+            }
+                catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        return numberOfPointRecieved;
     }
 }
